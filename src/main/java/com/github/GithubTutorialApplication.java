1576e47549cb9a101eb750257e74175f700a307b
siddhesh <siddheshs871@gmail.com>
2022-12-01 10:24:57 +0530

    first commit

---
+++ src/main/java/com/github/GithubTutorialApplication.java
@@ -0,0 +1,13 @@
+package com.github;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GithubTutorialApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GithubTutorialApplication.class, args);
+	}
+
+}
